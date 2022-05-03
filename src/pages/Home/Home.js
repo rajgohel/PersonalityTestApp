5284ba1989e4cbae0314ca8168a8bd91fe83ce74
Raj Gohel <rajgohel0001@gmail.com>
2022-05-03 09:12:38 +0530

    style changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import { makeStyles } from '@mui/styles';
+// import { makeStyles } from '@mui/styles';
 import Button from '@mui/material/Button';
+import { Wrapper, Content } from './style';
 
-const useStyles = makeStyles({
-    wrapper: {
-        height: '63.8vh',
-        background: '#E2E1E0',
-        padding: '7rem 28rem',
+// const useStyles = makeStyles({
+//     wrapper: {
+//         height: '63.8vh',
+//         background: '#E2E1E0',
+//         padding: '7rem 28rem',
 
-    },
-    content: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        background: '#fff',
-        boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-        padding: '2rem'
-    },
-});
+//     },
+//     content: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         flexDirection: 'column',
+//         background: '#fff',
+//         boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+//         padding: '2rem'
+//     },
+// });
 
 const Home = () => {
-    const classes = useStyles();
+    // const classes = useStyles();
     return (
-        <div className={classes.wrapper}>
-            <div className={classes.content}>
+        <Wrapper>
+            <Content>
                 <h2>Who Am I?</h2>
                 <p>An in-depth personality test designed by psychologist</p>
                 <Button variant="contained">Get started</Button>
-            </div>
-        </div>
+            </Content>
+        </Wrapper>
     )
 }
 
---
+++ src/pages/Home/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 90vw;
+    max-width: 500px;
+    margin: 4rem auto;
+    background: rgba(255, 255, 255, 0.664);
+    border-radius: 0.25rem;
+    padding: 3rem;
+`;
+
+export { Wrapper, Content };
